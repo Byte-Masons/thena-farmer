cc585e78f233e9ae53d4d9de3278335d436f15c4
imrtlfarm <imrtlfarm@gmail.com>
2023-01-13 19:26:26 -0800

    add keeper to deploy script

--- scripts/deploy/2-strategy.js
+++ scripts/deploy/2-strategy.js
@@ -16,6 +16,7 @@ async function main() {
   const superAdmin = '0x90c75c11735A7eeeD06E993fC7aF6838d86A1Ba7';
   const admin = '0xC17DfA7Eb4300871D5f022c107E07F98c750472e';
   const guardian = '0x30d65Ae22BBbe44208Dd8964DDE31Def0Fc1B9ee';
+  const keeper1 = '';
 
   const strategy = await hre.upgrades.deployProxy(
     Strategy,
@@ -24,6 +25,7 @@ async function main() {
       treasuryAddress,
       [strategist1, strategist2, strategist3, strategist4],
       [superAdmin, admin, guardian],
+      [keeper1],
       gauge
     ],
     {kind: 'uups', timeout: 0},
