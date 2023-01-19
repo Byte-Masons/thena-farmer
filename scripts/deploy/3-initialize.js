d11523ef14e7d0554b7d9f9bfde0f03cc9d144dc
imrtlfarm <imrtlfarm@gmail.com>
2023-01-18 21:26:48 -0800

    post deploy

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = '0x5f3b1E13BE5d48D4FA0a6D922845931709DFc455';
+  const strategyAddress = '0xE289916E103489F8eaE2887C8bbb16E8E01CaFF2';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
--- test/starter-test.js
+++ test/starter-test.js
@@ -154,8 +154,8 @@ describe('Vaults', function () {
 
     //approving LP token and vault share spend
     await want.connect(wantHolder).approve(vault.address, ethers.constants.MaxUint256);
-    await strategy.connect(wantHolder).setTHEToRelayPath([veloAddress, joinErcAddress]);
-    await strategy.connect(wantHolder).setTHEToBUSDPath([veloAddress, usdcAddress]);
+    //await strategy.connect(wantHolder).setTHEToRelayPath([veloAddress, joinErcAddress]);
+    //await strategy.connect(wantHolder).setTHEToBUSDPath([veloAddress, usdcAddress]);
   });
 
   describe('Deploying the vault and strategy', function () {
