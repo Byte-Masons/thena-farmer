22520b9811d773cbe51e9e8c3166716d728e9210
imrtlfarm <imrtlfarm@gmail.com>
2023-01-18 21:13:10 -0800

    post deploy

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = '0x02417AC802741F81d84e9E52E524433fce1fCf3e';
+  const strategyAddress = '0xCE1a9cF9266b03404c43C8B0Da1ae833A6F922c7';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
