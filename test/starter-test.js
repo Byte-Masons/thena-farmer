c88f6f815047cc5f2fae0a4daddb9d13325e4dd6
imrtlfarm <imrtlfarm@gmail.com>
2023-01-20 11:51:42 -0800

    propogate and pass test

--- test/starter-test.js
+++ test/starter-test.js
@@ -154,7 +154,9 @@ describe('Vaults', function () {
 
     //approving LP token and vault share spend
     await want.connect(wantHolder).approve(vault.address, ethers.constants.MaxUint256);
-
+    await strategy.connect(strategist).setRelay(usdcAddress);
+    await strategy.connect(strategist).setTHEToRelayPath([veloAddress, joinErcAddress, usdcAddress]);
+    await strategy.connect(strategist).setTHEToBUSDPath([veloAddress, joinErcAddress, usdcAddress]);
   });
 
   describe('Deploying the vault and strategy', function () {
